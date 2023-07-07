cbad1d15af166f103284bbdbc2ec482a1cab7448
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-07-07 14:34:10 +0330

    commit

---
+++ src/pages/Payment/[price].tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const Payment = () => {
+  const router = useRouter();
+  console.log(router.query.price);
+  const price = router.query.price;
+
+  const handleSuccess = () => {
+    router.replace(`http://localhost:3000/payment/Success/${price}`);
+  };
+  const handleCancel = () => {
+    router.replace(`http://localhost:3000/payment/Cancel/${price}`);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen" dir="rtl">
+      <div className="flex flex-col gap-4">
+        <div>
+          <h2>
+            هزینه خرید شما برابر است با : <span>{price}</span>
+          </h2>
+        </div>
+
+        <div className="flex justify-between">
+          <button
+            onClick={() => handleSuccess()}
+            className="bg-green-600 hover:bg-green-700 py-2 rounded-xl w-5/12 text-white"
+          >
+            پرداخت
+          </button>
+          <button
+            className="bg-red-600 hover:bg-red-700 text-white rounded-xl py-2 w-5/12"
+            onClick={() => handleCancel()}
+          >
+            انصراف
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Payment;
